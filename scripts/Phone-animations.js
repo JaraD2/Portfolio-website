7cd9e983a99c1d6dde4f4268f585ab79c55d06c6
Jara Doornbos <81758642+JaraD2@users.noreply.github.com>
2023-11-17 20:16:24 +0100

    removed non-relevent directories

--- scripts/Phone-animations.js
+++ scripts/Phone-animations.js
@@ -1,4 +1,4 @@
 document.getElementById("nav-menu").addEventListener("click", function() {
     document.getElementById("nav-menu").classList.toggle("active");
-    document.getElementsById("test").style.display = "grid";
+    document.getElementById("menu").classList.toggle("active");
 });
