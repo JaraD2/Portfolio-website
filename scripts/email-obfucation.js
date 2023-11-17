463c4e3088b8e009c152a3cd12154daaaa46c875
Jara Doornbos <81758642+JaraD2@users.noreply.github.com>
2023-11-17 22:01:30 +0100

    Introduced script to obfucate email address

---
+++ scripts/email-obfucation.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Use JavaScript to display the email address
+    var user = 'jaradoornbos1';
+    var domain = 'gmail.com';
+    var email = user + '@' + domain;
+  
+    // Find the element with the ID "email"
+    document.getElementById('email-obfucation').innerHTML = email;
+  
+
+  });
+  
